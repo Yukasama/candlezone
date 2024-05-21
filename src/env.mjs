ae767d39b540afb6b6f86c2cce3454bbfa8f1d05
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-05-21 23:25:44 +0200

    Add direct url env, remove await env from next config

--- src/env.mjs
+++ src/env.mjs
@@ -31,6 +31,7 @@ export const env = createEnv({
     LOG_LEVEL: process.env.LOG_LEVEL,
     CRON_SECRET: process.env.CRON_SECRET,
     DATABASE_URL: process.env.DATABASE_URL,
+    DIRECT_URL: process.env.DIRECT_URL,
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
