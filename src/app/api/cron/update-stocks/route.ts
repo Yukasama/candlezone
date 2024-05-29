5e4a929b2a491698a81492f23c62d6982c7fc00b
Yukasama <53038817+Yukasama@users.noreply.github.com>
2024-05-29 10:42:55 +0200

    Add security to cron job

--- src/app/api/cron/update-stocks/route.ts
+++ src/app/api/cron/update-stocks/route.ts
@@ -3,7 +3,16 @@ import { logger } from '@/lib/logger'
 
 export const maxDuration = 1000 * 60 * 2
 
-export async function GET() {
+export async function GET({ req }: { req: Request }) {
+  const authToken = (req.headers.get('authorization') ?? '')
+    .split('Bearer ')
+    .at(1)
+
+  // If not found OR the bearer token does NOT equal the CRON_SECRET
+  if (!authToken || authToken != process.env.CRON_SECRET) {
+    return new Response('Unauthorized', { status: 401 })
+  }
+
   try {
     await uploadStocks({})
     logger.info('CRON-upload-stocks (done)')
