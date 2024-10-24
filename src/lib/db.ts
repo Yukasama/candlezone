e56736a871af0e04953b0d2c7837e229b8144b15
Yukasama <yukasamaa@gmail.com>
2024-10-24 15:45:20 +0200

    fix: remove edge db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,23 @@
-import { env } from '@/env.mjs';
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
 import 'server-only';
-import ws from 'ws';
 
-neonConfig.webSocketConstructor = ws;
-const connectionString = `${env.DATABASE_URL}`;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
-export const db = new PrismaClient({ adapter });
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+export const db = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalThis.prismaGlobal = db;
+}
+
+// neonConfig.webSocketConstructor = ws;
+// const connectionString = `${env.DATABASE_URL}`;
+
+// const pool = new Pool({ connectionString });
+// const adapter = new PrismaNeon(pool);
+// export const db = new PrismaClient({ adapter });
