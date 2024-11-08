eafa4a06e46ef5c07a0851a50f0768c4db0241e2
Yukasama <yukasamaa@gmail.com>
2024-11-08 23:54:49 +0100

    fix: remove prefetch from searchbar

--- src/features/shared/searchbar/searchbar-mobile.tsx
+++ src/features/shared/searchbar/searchbar-mobile.tsx
@@ -97,7 +97,6 @@ export const SearchbarMobile = ({ recentStocks = [] }: Readonly<Props>) => {
                   <Link
                     key={'recentlyviewed' + stock.symbol}
                     href={`/stocks/${stock.symbol}`}
-                    prefetch={true}
                   >
                     <CommandItem value={stock.symbol + stock.companyName}>
                       <SymbolItem stock={stock} size="sm" />
@@ -116,7 +115,6 @@ export const SearchbarMobile = ({ recentStocks = [] }: Readonly<Props>) => {
                 <Link
                   key={'search-command' + stock.symbol}
                   href={`/stocks/${stock.symbol}`}
-                  prefetch={true}
                 >
                   <CommandItem value={stock.symbol + stock.companyName}>
                     <SymbolItem stock={stock} size="sm" />
