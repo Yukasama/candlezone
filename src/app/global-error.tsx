d93cb22be30b7e0afc32aec604a2972fec7577aa
Yukasama <yukasamaa@gmail.com>
2024-11-14 17:56:38 +0100

    chore: change error to global error

---
+++ src/app/global-error.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { RotateCw } from 'lucide-react';
+import type NextError from 'next/error';
+
+interface Props {
+  readonly error: NextError & { digest?: string };
+  readonly reset: () => void;
+}
+
+export default function GlobalError({ reset }: Props) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="f-col mt-[360px] items-center gap-3">
+          <div className="f-col items-center">
+            <h2 className="text-lg">Oops! Something went wrong.</h2>
+            <p className="text-sm text-gray-400">
+              There was an error on our end.
+            </p>
+          </div>
+          <Button onClick={() => reset()}>
+            <RotateCw size={18} />
+            Reload page
+          </Button>
+        </div>
+      </body>
+    </html>
+  );
+}
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,3 +1,4 @@
+import { env } from '@/env.mjs';
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
@@ -7,5 +8,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
+    sitemap: new URL('/sitemap.xml', env.NEXT_PUBLIC_HOST_URL).href,
   };
 }
