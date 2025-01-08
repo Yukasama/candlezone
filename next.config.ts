36de37712778cbaaf51d5d46051d20695608d096
Yukasama <yukasamaa@gmail.com>
2025-01-08 11:44:35 +0100

    fix: settings mobile hover, change to canary (ppr)

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   serverExternalPackages: ['pino', 'pino-pretty'],
   experimental: {
     reactCompiler: true,
+    ppr: true,
   },
   images: {
     remotePatterns: [
--- src/features/settings/settings-link.tsx
+++ src/features/settings/settings-link.tsx
@@ -18,7 +18,7 @@ export const SettingsLink = ({ icon, href, label }: Props) => {
     <Link
       href={href}
       className={cn(
-        'f-center h-9 w-full justify-center gap-2 rounded-full px-3 text-muted-foreground hover:text-primary lg:justify-start hover:lg:bg-accent',
+        'f-center h-9 w-full justify-center gap-2 rounded-full px-3 text-muted-foreground lg:justify-start hover:lg:bg-accent hover:lg:text-primary',
         pathname === href && 'bg-accent text-primary',
       )}
     >
